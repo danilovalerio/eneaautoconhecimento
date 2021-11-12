daffff98ec9a711ae20588138e78c92563ca0f22
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-11-12 17:42:52 -0300

    estilizacao do widget question

--- lib/question.dart
+++ lib/question.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class QuestionWidget extends StatelessWidget {
-
   final String texto;
 
   const QuestionWidget({Key? key, required this.texto}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        texto,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
